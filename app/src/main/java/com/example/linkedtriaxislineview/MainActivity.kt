eae9a9164d2da618d0815e7f822bf2bf045c587b
Anwesh43 <anweshthecool0@gmail.com>
2020-10-30 00:20:01 +0530

    adding project setup

---
+++ app/src/main/java/com/example/linkedtriaxislineview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.linkedtriaxislineview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
