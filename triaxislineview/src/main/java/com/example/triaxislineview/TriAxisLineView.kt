9ae7d69b8ce6775a1ae55d9668a7c06470b6475d
Anwesh43 <anweshthecool0@gmail.com>
2020-10-30 00:45:08 +0530

    adding Canvas extension method

--- triaxislineview/src/main/java/com/example/triaxislineview/TriAxisLineView.kt
+++ triaxislineview/src/main/java/com/example/triaxislineview/TriAxisLineView.kt
@@ -8,7 +8,8 @@ import android.graphics.Canvas
 import android.content.Context
 import android.app.Activity
 
-val parts : Int = 5
+val lines : Int = 3
+val parts : Int = lines + 2
 val lSizeFactor : Float = 3f
 val strokeFactor : Float = 90f
 val axisLineFactor : Float = 5.8f
@@ -24,8 +25,50 @@ val colors : Array<Int> = arrayOf(
     Color.parseColor(it)
 }.toTypedArray()
 val backColor : Int = Color.parseColor("#BDBDBD")
+val lWidth : Float = 0.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTriAxisLine(scale : Float, w : Float, h : Float , paint : Paint)  {
+    val size : Float = Math.min(w, h) / lSizeFactor
+    val lSize : Float = size / axisLineFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val lGap : Float = (lWidth * size) / lines
+    val offset : Float = (size - lGap * lines) / 2
+    val yOffset : Float = (size - lSize) / 2
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(-90f * sf2)
+        drawLine(0f, 0f, size * sf1, 0f, paint)
+        restore()
+    }
+    save()
+    translate(offset, -lSize / 4)
+    for (j in 0..(lines - 1)) {
+        val si : Int = j % 2
+        val sj : Float = 1f - 2 * si
+        val sfj : Float = sf.divideScale(2 + j, parts)
+        save()
+        translate(lGap * j, -yOffset)
+        drawLine(0f, 0f, lGap * sfj, -lSize * sfj * sj, paint)
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawTALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriAxisLine(scale, w, h, paint)
+}
