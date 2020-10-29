1b94216087205fa9296b3e7fdcfe5bc383a240f6
Anwesh43 <anweshthecool0@gmail.com>
2020-10-30 01:02:18 +0530

    adding renderer in view

--- triaxislineview/src/main/java/com/example/triaxislineview/TriAxisLineView.kt
+++ triaxislineview/src/main/java/com/example/triaxislineview/TriAxisLineView.kt
@@ -199,4 +199,27 @@ class TriAxisLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : TriAxisLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val tal : TriAxisLine = TriAxisLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            tal.draw(canvas, paint)
+            animator.animate {
+                tal.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            tal.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
