9c43a2dbbbe663c51df30fedcb32780d2ea09624
Anwesh43 <anweshthecool0@gmail.com>
2020-10-30 01:07:52 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedtriaxislineview/MainActivity.kt
+++ app/src/main/java/com/example/linkedtriaxislineview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedtriaxislineview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.triaxislineview.TriAxisLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriAxisLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
