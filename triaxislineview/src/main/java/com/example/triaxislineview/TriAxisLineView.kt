1377c9d69207cc224d8b59cef54f0994cd312c70
Anwesh43 <anweshthecool0@gmail.com>
2020-10-30 00:25:27 +0530

    adding constants and libraies

--- triaxislineview/src/main/java/com/example/triaxislineview/TriAxisLineView.kt
+++ triaxislineview/src/main/java/com/example/triaxislineview/TriAxisLineView.kt
@@ -1,2 +1,27 @@
 package com.example.triaxislineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val parts : Int = 5
+val lSizeFactor : Float = 3f
+val strokeFactor : Float = 90f
+val axisLineFactor : Float = 5.8f
+val delay : Long = 20
+val scGap : Float = 0.02f / parts
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#673AB7",
+    "#4CAF50",
+    "#FF9800"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+
